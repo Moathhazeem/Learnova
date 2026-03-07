0b101ce301739120e7dc68aa1127274a787864fe
Moathhazeem <moathhazeem661@gmail.com>
2026-03-07 13:15:16 +0200

    New update for sign in page

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,31 @@ import './App.css'
 import Header from './Header'
 import LandingPage from './LandingPage'
 import Footer from './Footer'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
 import SignIn from './sign_page/sign_in'
+function Layout({ children }) {
+  const location = useLocation();
+  const noheaderfooter = ["/sign_in"];
+  return (
+    <>
+      {!noheaderfooter.includes(location.pathname) && <Header />}
+      {children}
+      {!noheaderfooter.includes(location.pathname) && <Footer />}
+    </>
+  )
+}
 function App() {
   const [count, setCount] = useState(0)
-
   return (
     <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/sign_in" element={<SignIn />} />
-      </Routes>
-      <Footer />
+      <Layout>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/sign_in" element={<SignIn />} />
+        </Routes>
+      </Layout>
     </Router>
   )
 }
 
-export default App
+export default App;
--- src/sign_page/sign_in.jsx
+++ src/sign_page/sign_in.jsx
@@ -4,43 +4,51 @@ import "./sign_in.css";
 function SignIn() {
     return (
         <>
-            <div className="sign-in">
-                <h1 className="title-sign-in">Sign in to<label style={{ color: "#0089EA" }}>Learnova</label></h1>
-                <form>
-                    <div className="form-group">
-                        <label htmlFor="email">Email</label>
-                        <input type="email" id="email" name="email" required placeholder="exampleMoath_hazeem@gmail.com" />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="password">Password</label>
-                        <input type="password" id="password" name="password" required placeholder="!2##4$5^sdsds" />
-                    </div>
-                    <div className="form-options">
-                        <div className="form-group_remember">
-                            <input type="checkbox" id="remember" name="remember" />
-                            <label htmlFor="remember">Remember me</label>
+
+            <div className="sign-in-page-container">
+                <div className="sign-in">
+                    <h1 className="title-sign-in">Sign in to<label style={{ color: "#0089EA" }}>Learnova</label></h1>
+                    <form>
+                        <div className="form-group">
+                            <label htmlFor="email">Email</label>
+                            <input type="email" id="email" name="email" required placeholder="exampleMoath_hazeem@gmail.com" />
                         </div>
-                        <div className="form-group_forgot-password">
-                            <span style={{ color: "#0089EA", cursor: "pointer" }}>Forgot password</span>
+                        <div className="form-group">
+                            <label htmlFor="password">Password</label>
+                            <input type="password" id="password" name="password" required placeholder="example!2##4$5^sdsds" />
                         </div>
-                    </div>
-                    <button type="submit" className="sign-in-button">Sign In</button>
-                    <div className="or">
-                        <span htmlFor="or">Or sign in with</span>
-                    </div>
-                    <div className="social-media-container">
-                        <div className="social-button">
-                            <img src="./photo_icons/Google.png" alt="logo_Google" />
-                            <span>Google</span>
+                        <div className="form-options">
+                            <div className="form-group_remember">
+                                <input type="checkbox" id="remember" name="remember" />
+                                <label htmlFor="remember">Remember me</label>
+                            </div>
+                            <div className="form-group_forgot-password">
+                                <span style={{ color: "#0089EA", cursor: "pointer" }}>Forgot password</span>
+                            </div>
                         </div>
-                        <div className="social-button">
-                            <img src="./photo_icons/Facebook_Logo.png" alt="logo_Facebook" />
-                            <span>Facebook</span>
+                        <button type="submit" className="sign-in-button">Sign In</button>
+                        <div className="or">
+                            <span htmlFor="or">Or sign in with</span>
                         </div>
-                    </div>
-                    <p style={{ color: "#514C4Ca5" }}>Don't have an account? <a href="#" style={{ color: "#0089EA", fontWeight: "bold" }}>Sign Up</a></p>
-                </form >
-            </div >
+                        <div className="social-media-container">
+                            <div className="social-button">
+                                <img src="./photo_icons/Google.png" alt="logo_Google" />
+                                <span>Google</span>
+                            </div>
+                            <div className="social-button">
+                                <img src="./photo_icons/Facebook_Logo.png" alt="logo_Facebook" />
+                                <span>Facebook</span>
+                            </div>
+                        </div>
+                        <div className='DHAA'>
+                            <p style={{ color: "#514C4Ca5" }}>Don't have an account? <a href="#" style={{ color: "#0089EA", fontWeight: "bold" }}>Sign Up</a></p>
+                        </div>
+                    </form >
+                </div >
+                <div className="Photo">
+                    <img src="./photo/photo_sign_in.jpg" alt="Photo related to sign in" title="Photo related to sign in" />
+                </div>
+            </div>
         </>
     )
 }
