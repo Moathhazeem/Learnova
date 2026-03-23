9d0143932db56a8905954cf12b6d68b7427990f4
Moathhazeem <moathhazeem661@gmail.com>
2026-03-23 16:22:15 +0200

    Add menu for header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import "./Header.css"
 import { useNavigate } from "react-router-dom";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -9,13 +9,31 @@ function Header() {
   const [search, setSearch] = useState("")
   const location = useLocation();
   const isLandingPage = location.pathname === "/" || location.pathname === "/LandingPage";
+  const isSettingPage = location.pathname === "/Setting/Profile" || location.pathname === "/Setting/Security" || location.pathname === "/Setting/Preferences" || location.pathname === "/Setting/Privacy" || location.pathname === "/Setting/Notification" || location.pathname === "/Setting/Payment";
 
   const handleSearch = (e) => {
     setSearch(e.target.value)
   }
-
+  const [isOpen, setIsOpen] = useState(false);
+  const menuRef = useRef(null);
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  }
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+  }, [])
   return (
     <>
+
+
       <header>
         <div className="header-left">
           {isLandingPage ? (
@@ -24,12 +42,30 @@ function Header() {
               <button className="log-in" onClick={() => Navigate("/log_in")}>Log In</button>
             </div>
           ) : (
-            <div className="account">
-              <img src="/photo_icons/account.png" alt="Account icon" />
-              <img src="/photo_icons/notification.png" alt="Notification icon" />
-            </div>
+            <>
+              <div className="account" ref={menuRef}>
+                <img src="/photo_icons/account.png" alt="Account icon" onClick={toggleMenu} />
+                <img src="/photo_icons/notification.png" alt="Notification icon" />
+              </div>
+              {isOpen && (
+                <div className="Menu">
+                  <ul style={{ listStyleType: "none", padding: "0", margin: "0" }}>
+                    <li style={{ padding: "8px 0", cursor: "pointer" }}>Profile</li>
+                    <li style={{ padding: "8px 0", cursor: "pointer" }}>My learning</li>
+                    <li style={{ padding: "8px 0", cursor: "pointer" }}>My Purchases</li>
+                    <li style={{ padding: "8px 0", cursor: "pointer" }}>Explore</li>
+                    <li style={{ padding: "8px 0", cursor: "pointer" }}>Settings</li>
+                    <li style={{ padding: "8px 0", cursor: "pointer" }}>Help center</li>
+                    <li style={{ padding: "8px 0", cursor: "pointer" }}>Logout</li>
+                  </ul>
+                </div>
+              )}
+            </>
           )}
         </div>
+
+
+
 
         <div className="header-right">
           <div className="search-container">
