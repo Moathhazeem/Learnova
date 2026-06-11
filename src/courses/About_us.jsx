8fc1699354aa403765ff393bfa2b9a689c0c5ed0
Moathhazeem <moathhazeem661@gmail.com>
2026-06-11 10:10:48 +0300

    add achievements and truest student componet

--- src/courses/About_us.jsx
+++ src/courses/About_us.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./About_us.css";
 import { useTranslation } from "react-i18next";
 import { Link, useLocation } from "react-router-dom";
+import { Users, BookOpen, UserCheck, Clock, Award } from "lucide-react";
 
 function AboutUs() {
     const { t } = useTranslation();
@@ -56,6 +57,52 @@ const StoryLearnova = [
         ]
     }
 ];
+const TrustStudent = [
+    {
+        id: 1,
+        name: "Ali Moath",
+        description: "Learnova made the learning experience fun and easy for me. I loved the way the courses were organized, with interactive content and short videos that helped me understand quickly.",
+        image: "/Photo/man_1.jpg"
+    },
+    {
+        id: 2,
+        name: "Sarah Ahmed",
+        description: "The courses on Learnova are excellent and reliable, and I understood many concepts that I struggled with on other platforms. I loved the practical exercises and real-life examples that help to apply what I learned directly.",
+        image: "Photo/women_1.jfif"
+    },
+    {
+        id: 3,
+        name: "Mohammed Ali",
+        description: "The best thing about Learnova is that I can learn at my own pace without pressure. The platform allows me to return to the material and review it many times, and this has made me more confident while learning new skills.",
+        image: "/Photo/man_2.jpg"
+    },
+    {
+        id: 4,
+        name: "Aisha Mohamed",
+        description: "The trainers are professional and the content is wonderfully organized, with practical examples that make learning fun and realistic. I advise anyone who wants to develop their skills to start with Learnova immediately.",
+        image: "Photo/women_2.jfif"
+    },
+    {
+        id: 5,
+        name: "Mohammed Salah",
+        description: "I learned new skills on Learnova that are useful in my work and have boosted my self-confidence. The wide variety of courses and different ways of presenting content make learning more engaging and enjoyable..",
+        image: "/Photo/man_3.jpg"
+    },
+    {
+        id: 6,
+        name: "Aya Mohamed",
+        description: "The Learnova platform is flexible and easy to use. I loved the learning experience on it because it combines quality and flexibility. The videos, additional resources, and practical lessons helped me fully understand.",
+        image: "Photo/women_3.jfif"
+    },
+
+]
+const Achievements = [
+    { id: 1, icon: <Users size={32} />,     number: "5000+",  description: "The number of students currently registered on the platform." },
+    { id: 2, icon: <BookOpen size={32} />,  number: "120+",   description: "Total number of courses available in all fields." },
+    { id: 3, icon: <UserCheck size={32} />, number: "50+",    description: "The number of trainers and experts who offer the courses." },
+    { id: 4, icon: <Clock size={32} />,     number: "10000+", description: "Total educational hours available to students." },
+    { id: 5, icon: <Award size={32} />,     number: "1500+",  description: "Number of certificates awarded to students after completing the courses." }
+];
   return (
    <div className="about-us-container">
        {/* Breadcrumbs */}
@@ -106,7 +153,36 @@ const StoryLearnova = [
                                     ))}
                                 </div>
                             </div>
+                            <div className="Trust_student">
+                                <h1 className="Trust_title">{t("setting.about_us_trust_title", "Our trusted Students")}</h1>
+                                <div className="Trust_student_container">
+                                    {TrustStudent.map((student) => (
+                                        <div className="student_card" key={student.id}>
+                                            <p className="student_description">{student.description}</p>
+                                            <div className="student_container">
+                                                <img src={student.image} alt={student.name} className="student_image" />
+                                                <p className="student_name">{student.name}</p>
+                                            </div>
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
+                            <div className="our-achievements">
+                                <h1 className="Achievements_title">{t("setting.about_us_achievements_title", "Our Achievements")}</h1>
+                                <div className="Achievements_container">
+                                    {Achievements.map((achievement) => (
+                                        <div className="achievement-card" key={achievement.id}>
+                                            <div className="achievement-IcNu-container">
+                                            <div className="achievement-icon">{achievement.icon}</div>
+                                            <h3 className="achievement-number">{achievement.number}</h3>
+                                            </div>
+                                            <p className="achievement-description">{achievement.description}</p>
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
                         </div>
+                        
     </div>
 
 
